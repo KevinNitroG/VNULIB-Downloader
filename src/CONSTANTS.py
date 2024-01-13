8f207d3ee56924ad41ccd73516f183a4b7952571
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-01-14 02:08:52 +0700

    🎨 Re-construct source code structure

--- src/CONSTANTS.py
+++ src/CONSTANTS.py
@@ -1,6 +1,6 @@
-# ✨ VNULIB DOWNLOADER ✨
-
-<pre align="center">
+AUTHOR: str = "KevinNitroG & NTGNguyen"
+VERSION: str = "0.1-beta"
+BANNER: str = """
 ██╗   ██╗███╗   ██╗██╗   ██╗██╗     ██╗██████╗ 
 ██║   ██║████╗  ██║██║   ██║██║     ██║██╔══██╗
 ██║   ██║██╔██╗ ██║██║   ██║██║     ██║██████╔╝
@@ -14,24 +14,4 @@
 ██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║██╔══╝  ██╔══██╗
 ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝███████╗██║  ██║
 ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
-
-Python script download sách từ <strong>VNULIB</strong>
-</pre>
-
-[![GitHub last commit (by committer)](https://img.shields.io/github/last-commit/KevinNitroG/VNULIB-Downloader?style=for-the-badge&color=CAEDFF)](../../commits/main)
-![GitHub issues](https://img.shields.io/github/issues-raw/KevinNitroG/VNULIB-Downloader?style=for-the-badge&color=ffadad)
-![GitHub closed issues](https://img.shields.io/github/issues-closed/KevinNitroG/VNULIB-Downloader?style=for-the-badge&color=%23ffc6ff)
-![GitHub repo size](https://img.shields.io/github/repo-size/KevinNitroG/VNULIB-Downloader?style=for-the-badge&color=D8B4F8)
-[![GitHub contributors](https://img.shields.io/github/contributors/KevinNitroG/VNULIB-Downloader?style=for-the-badge&color=FBF0B2)](../../graphs/contributors)
-[![CodeFactor Grade](https://img.shields.io/codefactor/grade/github/KevinNitroG/VNULIB-Downloader?style=for-the-badge)](https://www.codefactor.io/repository/github/kevinnitrog/VNULIB-Downloader)
-[![wakatime](https://wakatime.com/badge/user/018b410d-fa7b-44ba-a5de-f025fcbeb499/project/018d034e-ab72-4111-95fa-bd5dc58c6ae7.svg?style=for-the-badge)](https://wakatime.com/badge/user/018b410d-fa7b-44ba-a5de-f025fcbeb499/project/018d034e-ab72-4111-95fa-bd5dc58c6ae7)
-
-> [!CAUTION]
->
-> **WORK IN PROGRESS**
->
-> Tạo repo vậy chứ chưa học gì hớt. Học đã rồi làm
-
-## 😌 CREDIT
-
--   Lấy cảm hứng từ [vnulib_downloader](https://github.com/tlatonf/vnulib_downloader/)
+""".strip("\n")
