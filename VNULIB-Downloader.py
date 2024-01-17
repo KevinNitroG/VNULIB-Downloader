30f8b7f486b123400ffcb3c7f72742ee4e5992d6
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-01-17 21:07:51 +0700

    ✨ Setup utils, Intro

--- VNULIB-Downloader.py
+++ VNULIB-Downloader.py
@@ -1,9 +1,20 @@
-"""Main file to run VNULIB Downloader"""
+"""VNULIB Downloader"""
+
+from src.utils import SYSTEM_PAUSE
+from src.utils.printIntro import printIntro
 
 
 def main() -> None:
-    """Main function"""
-    pass
+    """Main function
+
+    Params:
+        - None
+
+    Returns:
+        - None
+    """
+    printIntro()
+    SYSTEM_PAUSE()
 
 
 if __name__ == '__main__':
---
+++ src/utils/printIntro.py
@@ -0,0 +1,62 @@
+"""Print out the banner, authors, version"""
+
+
+from os import get_terminal_size
+from print_color.print_color import print as printColor
+from ..utils import TERMINAL_SIZE_COLUMNS
+from ..CONSTANTS import BANNER, AUTHORS, VERSION
+
+
+def printBanner(lines: list[str]) -> None:
+    """Print out the banner
+
+    Params:
+        - lines (str): List of lines of the banner
+
+    Returns:
+        - None
+    """
+    for line in lines:
+        printColor(line.center(TERMINAL_SIZE_COLUMNS),
+                   color='purple')
+
+
+def printAuthors(authors: str) -> None:
+    """Print out the authors
+
+    Params:
+        - authors (str): Authors
+
+    Returns:
+        - None
+    """
+    to_print_authors: str = f'\033[94m\033[43m {authors} \033[0m'
+    print(to_print_authors.rjust(TERMINAL_SIZE_COLUMNS))
+
+
+def printVersion(version: str) -> None:
+    """Print out the version
+
+    Params:
+        - version (str): Version
+
+    Returns:
+        - None
+    """
+    to_print_version: str = f'\033[45m\033[44m {version} \033[0m'
+    print(to_print_version.rjust(TERMINAL_SIZE_COLUMNS))
+
+
+def printIntro() -> None:
+    """Print out the banner, authors, version
+
+    Params:
+        - None
+
+    Returns:
+        - None
+    """
+    printBanner(lines=[line for line in BANNER.split('\n')])
+    printAuthors(authors=AUTHORS)
+    printVersion(version=VERSION)
+    print()
