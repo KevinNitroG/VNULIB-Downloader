7cf05343e09a00de1d988716bec54cfbe0675f8e
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-01-18 21:44:07 +0700

    👷 Strictly check before releasing

---
+++ script/checkRelease.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION=$(cat "src/CONSTANTS.py" | grep VERSION | cut -d "=" -f 2 | sed 's/'\''//g' | sed 's/ //g' | sed 's/\r//')
+echo "Current version: $VERSION"
+echo "VERSION=$VERSION" >>"$GITHUB_OUTPUT"
+
+REMOTE_VERSIONS=$(git ls-remote --tags | cut -d "/" -f 3)
+echo "Remote versions: $REMOTE_VERSIONS"
+
+for REMOTE_VERSION in $REMOTE_VERSIONS; do
+    if [[ "$REMOTE_VERSION" == *"$VERSION"* ]]; then
+        echo "Current version is already released."
+        echo "CONTINUE=false" >>"$GITHUB_OUTPUT"
+        exit 0
+    fi
+done
+
+echo "CONTINUE=false" >>"$GITHUB_OUTPUT"
+
+if [[ $VERSION == *"beta"* ]]; then
+    echo "SET_PRE_RELEASE=true" >>"$GITHUB_OUTPUT"
+else
+    echo "SET_PRE_RELEASE=false" >>"$GITHUB_OUTPUT"
+fi
