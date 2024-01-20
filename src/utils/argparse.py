6b0fd7845575005c321c3982211d3e9aa103b0bc
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-01-21 02:39:54 +0700

    🎨 Utils: Migrate Kevin's funcs

--- src/utils/argparse.py
+++ src/utils/argparse.py
@@ -1,21 +1,6 @@
 from argparse import ArgumentParser, Namespace
 
 
-parser = ArgumentParser()
-parser.add_argument('--links', type=list, nargs='+',
-                    help='Links of the book(s) to be downloaded')
-parser.add_argument('--overwrite-book', '-o', action='store_true', default=None,
-                    help='If the folder of images / PDF file of the book is already exist, then overwrite it')
-parser.add_argument('--create-pdf', '-pdf', action='store_true',
-                    default=None, help='Merge images to a PDF')
-parser.add_argument('--keep-imgs', '-k', action='store_true',
-                    default=None, help='Keep images after merging to PDF')
-parser.add_argument('--log', '-l', action='store_true',
-                    default=False, help='Log the processed books information to logs folder')
-
-args: Namespace = parser.parse_args()
-
-
 def argParse() -> Namespace:
     """Parse the arguments
 
@@ -25,4 +10,16 @@ def argParse() -> Namespace:
     Returns:
         - None
     """
+    parser = ArgumentParser()
+    parser.add_argument('--links', type=list, nargs='+',
+                        help='Links of the book(s) to be downloaded')
+    parser.add_argument('--overwrite-book', '-o', action='store_true', default=None,
+                        help='If the folder of images / PDF file of the book is already exist, then overwrite it')
+    parser.add_argument('--create-pdf', '-pdf', action='store_true',
+                        default=None, help='Merge images to a PDF')
+    parser.add_argument('--keep-imgs', '-k', action='store_true',
+                        default=None, help='Keep images after merging to PDF')
+    parser.add_argument('--log', '-l', action='store_true',
+                        default=False, help='Log the processed books information to logs folder')
+    args: Namespace = parser.parse_args()
     return args
