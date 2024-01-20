54d972ee1f50a6f36798bee7ef5cdd50984f483f
NTGNguyen <23521049@gm.uit.edu.vn>
2024-01-20 23:25:37 +0700

    ✅ Add 2 function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,8 @@
 """Contains utility functions for the project"""
 
 
-from os import makedirs, path
-from shutil import rmtree
+from os import makedirs, path, os
+from shutil import rmtree, shutil
 
 from src.utils.printColor import printInfo, printSuccess
 
@@ -39,3 +39,34 @@ def createDirectory(*directories: str, force: bool = False) -> None:
                     message=f'{directory} was already created. Skip creating it')
                 return None
         makedirs(name=directory)
+
+
+def deleteAllBooks(path: str) -> None:
+    """Delete All The Book's Folders
+
+    Args:
+        - path (str): The path include all Book's Folders
+
+    Returns:
+        - None
+    """
+    subdirectories = [os.path.join(path, d) for d in os.listdir(
+        path) if os.path.isdir(os.path.join(path, d))]
+
+    for subdirectory in subdirectories:
+        shutil.rmtree(subdirectory)
+
+
+def deleteAllJPGFile(path: str) -> None:
+    """Delete All JPG File
+
+        Args:
+            -path(str): The path include Images and PDF file
+
+        Returns:
+            -None
+    """
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.jpg'):
+                os.remove(os.path.join(root, file))
