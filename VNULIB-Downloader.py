7eafe4c9e653357300b813f035eaaf44ec6a3886
NTGNguyen <23521049@gm.uit.edu.vn>
2024-01-21 23:34:59 +0700

    💥 Add Some Function to main_file

--- VNULIB-Downloader.py
+++ VNULIB-Downloader.py
@@ -1,10 +1,13 @@
 """VNULIB Downloader"""
 
 
+import os
+
 from logging import Logger
 
-from src.utils import pause
-from src.utils.toolArgparse import argParse
+from src.utils.utils import (
+    pause, createDirectory, deleteAllBooks, deleteAllJPGFile)
+from src.modules.downloadImages import (dowloadAllImagesFromAllLinks)
 from src.utils.printIntro import printIntro
 from src.utils.printColor import printTitle
 from src.utils.setupVariables import setupVariables
@@ -28,11 +31,14 @@ def main() -> None:
         config_file=CONFIG_FILE)
     LOGGER: Logger | None = setupLogger(LOG=LOG)
     print(LINKS, OVERWRITE_BOOK, CREATE_PDF, KEEP_IMGS, LOG, LOGGER)
+    createDirectory('dowloaded_books')
+    if OVERWRITE_BOOK:
+        deleteAllBooks(os.getcwd())
+    dowloadAllImagesFromAllLinks(LINKS)
+    if not KEEP_IMGS:
+        deleteAllJPGFile(os.getcwd())
 
 
 if __name__ == '__main__':
-    if argParse().update:
-        pass  # implement later
-    else:
-        main()
-        pause()
+    main()
+    pause()
