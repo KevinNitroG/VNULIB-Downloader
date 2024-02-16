c682eb86504abc1b782ea914f5e434d578800f27
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-02-17 00:57:46 +0700

    🐛 Pyinstaller: MEIPASS add log config file

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,6 +1,7 @@
 """Setup the logger"""
 
 
+import sys
 from os import path, makedirs
 from logging import getLogger, Logger
 from logging.config import dictConfig
@@ -19,6 +20,8 @@ def setup_logger(config_path: str) -> Logger:
     """
     if not path.exists('VNULIB-Downloader/logs'):
         makedirs('VNULIB-Downloader/logs')
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        config_path = path.join(sys._MEIPASS, config_path)
     with open(config_path, 'r', encoding='utf-8') as config_file:
         dictConfig(safe_load(config_file))
     return getLogger("vnulib_downloader")
