68a4ea0ac06cc4f358f5f62466431f99cc49c988
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-02-18 21:53:12 +0700

    🎨 Chore: Logging format, docstring for Link class

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main() -> None:
     user_options.links = link_parse.parse()
     if link_parse.need_to_convert:
         logger.info('There is / are some link(s) need to be processed')
+        print_title(message='CONVERT LINKS')
         with Browser(browser=user_options.browser,
                      headless=user_options.headless) as driver:
             Login(driver=driver,
