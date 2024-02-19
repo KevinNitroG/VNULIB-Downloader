3906b7ded580506f06a2e93bff71daa2aa85b56f
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-02-19 18:50:49 +0700

    ♻️ print_title: remove kwarg

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ def main() -> None:
     """Main function to run VNULIB Downloader"""
     PrintIntro()
 
-    print_title(message='USER OPTIONS')
+    print_title('USER OPTIONS')
     Config()
     user_options = UserOptions()
     user_options.setup()
 
-    print_title(message='PARSE LINKS')
+    print_title('PARSE LINKS')
     link_parse = LinkParse(links=user_options.links)
     user_options.links = link_parse.parse()
     if link_parse.need_to_process:
         logger.info('There is / are some link(s) need to be processed')
-        print_title(message='PROCESS LINKS')
+        print_title('PROCESS LINKS')
         with Browser(browser=user_options.browser,
                      headless=user_options.headless) as driver:
             Login(driver=driver,
@@ -30,12 +30,11 @@ def main() -> None:
             user_options.links = LinkParse(links=user_options.links).parse()
             user_options.links = Action(driver=driver,
                                         links=user_options.links).action()
-    logger.debug(msg='LINKS OBJECT:\n'
-                 f'{pformat(user_options.links)}')
+    logger.debug(msg=f'LINKS OBJECT:\n{pformat(user_options.links)}')
 
-    print_title(message='DOWNLOAD')
+    print_title('DOWNLOAD')
 
-    print_title(message='PDF')
+    print_title('PDF')
 
 
 if __name__ == '__main__':
