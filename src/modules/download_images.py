9d8bc3c7aade4f2eb42b6c6a5013303bc80a58ec
NTGNguyen <23521049@gm.uit.edu.vn>
2024-02-20 09:33:29 +0700

    ✅ 🐛 Update download_image_from_page method signature,fixs some bugs

--- src/modules/download_images.py
+++ src/modules/download_images.py
@@ -1,11 +1,9 @@
 """Download book's images"""
 import os
-import re
 from io import BytesIO
 import requests
-
 from PIL import Image
-
+from PIL.Image import Image as ImagePIL
 from alive_progress import alive_bar
 from requests import Response
 from .link_parse import LinkFile, Link
@@ -43,8 +41,6 @@ class DownloadImages:
         Args:
             - link (str): link which that type is page link
 
-        Return:
-            - None
         """
         folder_name: str = link.name
         folder_path: str = os.path.join(download_directory, folder_name)
@@ -52,12 +48,10 @@ class DownloadImages:
         number_of_pages: int = link.num_pages
         with alive_bar(number_of_pages) as bar:  # pylint: disable=disallowed-name
             for page_num in range(number_of_pages):
-                sub_link = create_page_link(link.page_link)
-                sub_link: str = re.sub(
-                    r'page=\d+', f'page={page_num}', link.page_link)
+                sub_link: str = create_page_link(link.page_link, page_num)
                 image_bytes: bytes = DownloadImages.download_image_from_page(
                     sub_link)
-                image = Image.open(BytesIO(image_bytes))
+                image: ImagePIL = Image.open(BytesIO(image_bytes))
                 image_path: str = os.path.join(
                     download_directory, f"image_{page_num}.jpg")
                 image.save(image_path)
@@ -65,15 +59,12 @@ class DownloadImages:
 
     @staticmethod
     def download_images_from_preview_link(links: Link, download_directory: str) -> None:
-        """
-        Download images from the preview link.
+        """Download images from the preview link.
 
         Args:
             links (Link): The preview link.
             download_directory (str): The directory to save the downloaded images.
 
-        Returns:
-            None
         """
         folder_book_name: str = links.name
         folder_path: str = os.path.join(download_directory, folder_book_name)
