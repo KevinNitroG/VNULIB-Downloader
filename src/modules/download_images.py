ae2a048fbcbf915ab4345d9c477f38aad53b3f4c
NTGNguyen <23521049@gm.uit.edu.vn>
2024-02-23 21:14:26 +0700

    🐛 Fix the error in page loop:start_page=0-->start_page=1

--- src/modules/download_images.py
+++ src/modules/download_images.py
@@ -36,24 +36,23 @@ class DownloadImages:
 
     @staticmethod
     def download_images_from_page_link_or_preview_link(link: LinkFile, download_directory: str) -> None:
-        """Dowload All book's images from page link
+        """Dowload All book's images from page link and previews link
 
         Args:
-            - link (str): link which that type is page link
-
+            - link (LinkFile): link which that type is page link
         """
         folder_name: str = link.name
         folder_path: str = os.path.join(download_directory, folder_name)
         if create_directory(folder_path):
             number_of_pages: int = link.num_pages
             with alive_bar(number_of_pages) as bar:  # pylint: disable=disallowed-name
-                for page_num in range(number_of_pages):
+                for page_num in range(1, number_of_pages + 1):
                     sub_link: str = create_page_link(link.page_link, page_num)
                     image_bytes: bytes = DownloadImages.download_image_from_page(
                         sub_link)
                     image: ImagePIL = Image.open(BytesIO(image_bytes))
                     image_path: str = os.path.join(
-                        download_directory, f"image_{page_num}.jpg")
+                        download_directory, f'image_{page_num}.jpg')
                     image.save(image_path)
                     bar()  # pylint: disable=not-callable
 
@@ -64,7 +63,6 @@ class DownloadImages:
         Args:
             links (Link): The preview link.
             download_directory (str): The directory to save the downloaded images.
-
         """
         folder_book_name: str = links.name
         folder_path: str = os.path.join(download_directory, folder_book_name)
@@ -80,7 +78,6 @@ class DownloadImages:
             Args:
                 -links (list[Link]):List of Link
                 -download_directory(str):the directory save dowloads folder
-
         """
         for link in links:
             if link.original_type == 'page' or link.original_type == 'preview':
